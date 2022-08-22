1b6c9e284e28654538379db043ad55f5af58c92d
Bryson <bryson.h330@stu.nebo.edu>
2022-08-22 09:03:41 -0600

    Added unity the great beginning

---
+++ The great begining/Assets/scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     public int speed; // set speed
+     public float hInput; //horozintal speed 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
