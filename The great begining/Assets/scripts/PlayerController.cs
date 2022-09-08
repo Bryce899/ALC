3679b984d4a609795aa426c7deff8b033a9a4f88
Bryson <bryson.h330@stu.nebo.edu>
2022-09-08 08:35:21 -0600

    added jump

--- The great begining/Assets/scripts/PlayerController.cs
+++ The great begining/Assets/scripts/PlayerController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed; // set speed value
+    public float rotSpeed; //rotation
+    public float hInput; // Horizontal speed
+    public float vInput; // Vertical input
+    public float jumpForce; // jump height
+    public Rigidbody playerRB; // Reference Rigidbody compoent 
+
+    // update is called once per frame
+    void Update()
     {
-     public int speed; // set speed
-     public float hInput; //horozintal speed 
+
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime)
+        ;
+        
+        // Rotate Left and Right
+
+        transform.Translate(Vector3.forward * speed * vInput * Time.
+        deltaTime);//Forward and backward movement
+        if (Input.GetKeyDown(KeyCode.Space)) // check for space bar press
+            playerRB.AddForce(Vector3.up*jumpForce, ForceMode.Impulse); // makes player jump
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
