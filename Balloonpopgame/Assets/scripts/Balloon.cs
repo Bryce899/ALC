5681c539a924947df26bd3e4cdfac4326eea9e1f
Bryson <bryson.h330@stu.nebo.edu>
2022-09-14 09:05:16 -0600

    Fixed the if function for balloon game

--- Balloonpopgame/Assets/scripts/Balloon.cs
+++ Balloonpopgame/Assets/scripts/Balloon.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour
 {
-
-
     public int clickToPop = 3; // how many clicks before ballon pops
 
     public float scaleToIncrease = 0.10f; //scale increase each time the ballon is clicked
 
 
-
-
-
-
-
     // Start is called before the first frame update
     void Start()
-    { 
-
-
-        void OnMouseDown()
-        { 
-            //reduce clicks by one
-            clickToPop -= 1;
-
-            // Increase balloon volume
-            transform.localScale += Vector3.one *
-            scaleToIncrease;
-
-            if (clickToPop == 0)
+    {
 
 
 
+    }
 
-         }
+    void OnMouseDown()
+    {
+        //reduce clicks by one
+        clickToPop -= 1;
+
+        // Increase balloon volume
+        transform.localScale += Vector3.one *
+        scaleToIncrease;
+
+        if(clickToPop == 0)
+        {
+            // scoreManager.IncreaseScoreText(scoreToGive);
+            Destroy(gameObject);
+        }    
+     }
+
+
+
+         
 
 
     // Update is called once per frame
