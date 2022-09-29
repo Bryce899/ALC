3b882c2d883f5ac133c273c327ee4493d456e708
Bryson <bryson.h330@stu.nebo.edu>
2022-09-29 09:20:35 -0600

    made player move and added new sprites and boundaries

--- Rouge Jim/Assets/Scripts/Player_Controller.cs
+++ Rouge Jim/Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    public float speed; // how fast the player moves
+    private float hInput;// horizontal input
+    private float vInput;// vertical input
+
+
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector2.right * hInput * speed * Time.deltaTime)
+        ;// Move left and right
+
+        transform.Translate(Vector2.up * vInput * speed * Time.deltaTime);
+        // Move forward and back
     }
 }
+// Today is a good day :)
